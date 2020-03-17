fb6bb978110d33113d913e1d00b518388ac20e7f
Cassio Alves <cassiowin98@yahoo.com.br>
2020-03-17 17:37:30 -0300

    -Controllers

---
+++ AgendamentoClinico/Controllers/ClinicaController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace AgendamentoClinico.Controllers
+{
+    public class ClinicaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AgendamentoClinico/Controllers/PacienteController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace AgendamentoClinico.Controllers
+{
+    public class PacienteController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
